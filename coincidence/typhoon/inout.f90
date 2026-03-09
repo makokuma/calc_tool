74e220b009b52a1c39e624e5d7eafba44b9c4c1a
kumagai <meteokuma@gmail.com>
2026-03-09 17:26:20 +0900

    I added readcsv

--- coincidence/typhoon/inout.f90
+++ coincidence/typhoon/inout.f90
@@ -1,121 +1,109 @@
-    module inout
-        implicit none
-    contains
+module inout
+    implicit none
+    integer, parameter :: STRLEN = 256
+contains
 
-        subroutine readcsv(csvfile_name, sel_col_name)
-            !read csvfile (.dat)
-            character(len=*), intent(in):: csvfile_name
-            character(len=*), intent(in):: sel_col_name
-            integer :: csv_unit, ios
-            character(len=10000) :: line
-            character(len=:), allocatable :: header(:), sel_col(:), fields(:)
- 
+    subroutine open_csv(csvfile_name, csv_unit, ios)
+        character(len=*), intent(in) :: csvfile_name
+        integer, intent(out) :: csv_unit
+        integer, intent(out) :: ios
 
-            !read csv
-            open(newunit=csv_unit, file=trim(csvfile_name))
-            if (ios /= 0) stop 'open error'
+        open(newunit=csv_unit, file=trim(csvfile_name), &
+             status='old', action='read', iostat=ios)
+    end subroutine open_csv
 
-            !read header
-            read(csv_unit, '(A)', iostat=ios) line
-            if (ios /= 0) then
-                print *, 'header read error'
-                stop
-            endif
 
-            call split_csv_line(trim(line), header)
+    subroutine read_csv_row(csv_unit, fields, nfields, ios)
+        integer, intent(in) :: csv_unit
+        character(len=STRLEN), allocatable, intent(out) :: fields(:)
+        integer, intent(out) :: nfields
+        integer, intent(out) :: ios
 
-            !header information
-            sel_col = find_column(header, select_col_name)
+        character(len=10000) :: line
 
-            !read data
-            nline = 0
-            do
-                read(csv_unit, '(A)', iostat=ios) line
-                if (ios /= 0) exit
+        read(csv_unit, '(A)', iostat=ios) line
+        if (ios /= 0) then
+            nfields = 0
+            return
+        end if
 
-                call split_csv_line(trim(line), fields)
-                
-                if (sel_col > 0) then
-                    if (trim(fields(sel_col)) /= trim(select_value)) cycle
-                end if
-!                nline = nline + 1
+        call split_csv_line(trim(line), fields, nfields)
+    end subroutine read_csv_row
 
-                 
-!            do
-!              read(csv_unit, '(A)', iostat=ios) line
-!              if (ios /= 0) exit
-!              print *, trim(line)
-!            enddo
 
-            enddo
+    subroutine split_csv_line(line, fields, nfields)
+        character(len=*), intent(in) :: line
+        character(len=STRLEN), allocatable, intent(out) :: fields(:)
+        integer, intent(out) :: nfields
 
-            close(csv_unit)
-        end subroutine readcsv
+        integer :: i, start, lenline, k
+        integer, allocatable :: pos(:)
 
-        subroutine split_csv_line(line, fields)
-            character(len=*), intent(in) :: line
-            character(len=:), allocatable, intent(out) :: fields(:)
+        lenline = len_trim(line)
 
-            integer :: i, n, start, lenline, maxlen
-            integer, allocatable :: pos(:)
-            integer :: k
+        nfields = 1
+        do i = 1, lenline
+            if (line(i:i) == ',') nfields = nfields + 1
+        end do
 
-            lenline = len_trim(line)
+        allocate(fields(nfields))
+        fields = ''
 
-            n = 1
-            do i = 1, lenline
-              if (line(i:i) == ',') n = n + 1
-            enddo
-
-            allocate(pos(n-1))
+        if (nfields > 1) then
+            allocate(pos(nfields-1))
             k = 0
             do i = 1, lenline
-              if (line(i:i) == ',') then
-                  k = k + 1
-                  pos(k) = i
-              endif
-            enddo
+                if (line(i:i) == ',') then
+                    k = k + 1
+                    pos(k) = i
+                end if
+            end do
+        end if
 
-            maxlen = 1
-            start = 1
-            do i = 1, n-1
-              maxlen = max(maxlen, pos(i) - start)
-              start = pos(i) + 1
-            enddo
-            maxlen = max(maxlen, lenline - start + 1)
-
-            allocate(character(len=maxlen) :: fields(n))
-            fields = ''
-
-            start = 1
-            do i = 1, n-1
-              if (pos(i) >= start) then
+        start = 1
+        do i = 1, nfields-1
+            if (pos(i) >= start) then
                 fields(i) = adjustl(line(start:pos(i)-1))
-              else
-                  fields(i) = ''
-              endif
-              start = pos(i) + 1
-            enddo
-
-            if (start <= lenline) then
-              fields(n) = adjustl(line(start:lenline))
             else
-              fields(n) = ''
-            endif
-        end subroutine split_csv_line
+                fields(i) = ''
+            end if
+            start = pos(i) + 1
+        end do
 
-        integer function find_column(header, name)
-          character(len=*), intent(in) :: header(:)
-          character(len=*), intent(in) :: name
-          integer :: i
+        if (start <= lenline) then
+            fields(nfields) = adjustl(line(start:lenline))
+        else
+            fields(nfields) = ''
+        end if
 
-          find_column = -1
-          do i = 1, size(header)
-            if (trim(header(i)) == trim(name)) then
+        if (allocated(pos)) deallocate(pos)
+    end subroutine split_csv_line
+
+
+    integer function find_column(header, nheader, name)
+        character(len=*), intent(in) :: header(:)
+        integer, intent(in) :: nheader
+        character(len=*), intent(in) :: name
+        integer :: i
+
+        find_column = -1
+        do i = 1, nheader
+            if (trim(adjustl(header(i))) == trim(adjustl(name))) then
                 find_column = i
                 return
-            endif
-          enddo
-        end function find_column
-    
-    end module inout
+            end if
+        end do
+    end function find_column
+
+
+    function get_field(fields, nfields, idx) result(val)
+        character(len=*), intent(in) :: fields(:)
+        integer, intent(in) :: nfields, idx
+        character(len=STRLEN) :: val
+
+        val = ''
+        if (idx < 1 .or. idx > nfields) return
+        val = trim(fields(idx))
+    end function get_field
+
+end module inout
--- coincidence/typhoon/main.f90
+++ coincidence/typhoon/main.f90
@@ -1,10 +1,41 @@
-    program main
-        use inout, only: readcsv
-        implicit none
+program main
+    use inout, only: open_csv, read_csv_row, find_column, get_field, STRLEN
+    implicit none
 
-        !read csv
-        !HRA_info file
-        call readcsv('fort.100')
+    integer :: unit, ios
+    integer :: col_hrid, col_lat, col_lon
+    integer :: nheader, nfields, i
+    character(len=STRLEN), allocatable :: header(:), fields(:)
 
-        end program main
+    call open_csv('fort.100', unit, ios)
+    if (ios /= 0) stop 'open error'
 
+    call read_csv_row(unit, header, nheader, ios)
+    if (ios /= 0) stop 'header read error'
+
+    print *, 'nheader = ', nheader
+    do i = 1, nheader
+        print *, 'header(', i, ') = [', trim(header(i)), ']'
+    end do
+
+    col_hrid = find_column(header, nheader, 'hrid')
+    col_lat  = find_column(header, nheader, 'lat')
+    col_lon  = find_column(header, nheader, 'lon')
+
+    print *, 'col_hrid = ', col_hrid
+    print *, 'col_lat  = ', col_lat
+    print *, 'col_lon  = ', col_lon
+
+    if (col_hrid < 1 .or. col_lat < 1 .or. col_lon < 1) stop 'column not found'
+
+    do
+        call read_csv_row(unit, fields, nfields, ios)
+        if (ios /= 0) exit
+
+        print *, 'hrid = [', trim(get_field(fields, nfields, col_hrid)), ']'
+        print *, 'lat  = [', trim(get_field(fields, nfields, col_lat)), ']'
+        print *, 'lon  = [', trim(get_field(fields, nfields, col_lon)), ']'
+    end do
+
+    close(unit)
+end program main
