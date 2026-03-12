4992c64bfaebd7d61101bd97aa58777e6703b522
kumagai <meteokuma@gmail.com>
2026-03-12 22:58:40 +0900

    final version of judge typhoon

--- coincidence/typhoon/execute.sh
+++ coincidence/typhoon/execute.sh
@@ -11,7 +11,7 @@ set DST = 1
 set YEN = 2000
 set MEN = 10
 set DEN = 31
-set AREA = 'nihonkai'
+set AREA = 'taiheiyou'
 
 set ELEM = ('linear-stationary' 'linear' 'stationary' 'else')
 
@@ -22,28 +22,77 @@ set CDEN = `printf "%02d" $DEN`
 set DATE = $YST$CMST$CDST'-'$CMEN$CDEN
 
 set DATDIR = /mnt/jet12/makoto/extract_senjo/ext_sun_re/csv
-set CSVDIR = {$DATDIR}/{$THS}/{$AREA}'_'{$THS}'_25.csv'
-set RADIR = {$DATDIR}/rastat/${DATE}/$ELEM[$ELID]'_hras_stat_'{$THS}'_040.csv'
+set CSVDIR = ${DATDIR}/${THS}/${AREA}'_'${THS}'_25.csv'
+#set RADIR = {$DATDIR}/rastat/${DATE}/$ELEM[$ELID]'_hras_stat_'{$THS}'_040.csv'
 set TCDIR = /mnt/jet12/makoto/extract_senjo/environment/plot/py/data/Besttrack/bst_all.txt
 
 echo "CSVDIR = $CSVDIR"
-echo "RADIR  = $RADIR"
-echo "TCDIR  = $TCDIR"
+#echo "RADIR  = $RADIR"
+#echo "TCDIR  = $TCDIR"
 
 unlink fort.100
 unlink fort.200
 unlink fort.300
+unlink fort.400
 
-#CSV --> dat
+#CSV --> dat (const only)
 ln -s $CSVDIR 'fort.100'
-ln -s $RADIR 'fort.200'
 ln -s $TCDIR 'fort.300'
 
-#execute
-./judgeTD_tool 
+#fort.400 05d3 file
+set DATDIR_HRA = /mnt/jet12/makoto/extract_senjo/ext_sun_re/csv/${THS}/05d3/${THS}
+set DETAIL_BASENAME = ra03_5000m_recollect_${THS}_040.csv
 
+#fort.200 hra stat file
+set DATDIR2 = /mnt/jet12/makoto/extract_senjo/ext_sun_re/csv/rastat
+set DETAIL_BASENAME2 = linear-stationary_hras_stat_${THS}_040.csv
 
+#case file
+set CASEFILE = fort.100
 
+# dtst 
+set DTST_COL = 3
 
+#rm fort link(init) 
+rm -f fort.400
+rm -f fort.200
 
+# year all list of hra file
+set YEARS = (`awk -F, -v col=$DTST_COL 'NR>1 {print substr($col,1,4)}' $CASEFILE | sort -u`)
+
+echo "YEARS = $YEARS"
+
+foreach YEAR ($YEARS)
+
+    set DATE = ${YEAR}0401-1031
+    set HRAFILE = ${DATDIR_HRA}/${DATE}/${DETAIL_BASENAME}
+
+    if (! -e "$HRAFILE") then
+        echo "skip: file not found -> $HRAFILE"
+        continue
+    endif
+
+    set DATE = ${YEAR}0401-1031
+    set RASTATFILE = ${DATDIR2}/${DATE}/${DETAIL_BASENAME2}
+
+    if (! -e "$RASTATFILE") then
+        echo "skip: file not found -> $RASTATFILE"
+        continue
+    endif
+
+    rm -f fort.400
+    ln -s "$HRAFILE" fort.400
+
+    rm -f fort.200
+    ln -s "$RASTATFILE" fort.200
+
+    echo "======================================="
+    echo "YEAR    = $YEAR"
+    echo "fort.400 -> $HRAFILE"
+    echo "fort.200 -> $RASTATFILE"
+
+    #execute
+    ./judgeTD_tool $YEAR
+
+end
 
--- coincidence/typhoon/main.f90
+++ coincidence/typhoon/main.f90
@@ -1,41 +1,104 @@
 program main
-    use inout, only: open_csv, read_csv_row, find_column, get_field, STRLEN
+    use inout
+    use search
+    use calc_CE1
+    use calc_CE2
     implicit none
 
-    integer :: unit, ios
-    integer :: col_hrid, col_lat, col_lon
-    integer :: nheader, nfields, i
-    character(len=STRLEN), allocatable :: header(:), fields(:)
+    integer, parameter :: NMAX = 2000
+    integer :: ls_data, ios, nfields, nt, i
+    integer :: ce1_flag, ce2_flag
+    integer :: year_case
+    real :: min_dist, best_tylat, best_tylon
+    integer :: best_pres
 
-    call open_csv('fort.100', unit, ios)
-    if (ios /= 0) stop 'open error'
+    character(len=STRLEN), allocatable :: fields(:)
+    character(len=STRLEN) :: target_hrid
+    character(len=STRLEN) :: dtst
+    character(len=32) :: arg_year
+    character(len=8) :: hra_time(NMAX)
+    real :: hra_lat(NMAX), hra_lon(NMAX)
+    character(len=8) :: best_time
+    logical :: found_stats
+    real :: r1max, r3max, rtmax, racc, r1mean, r3mean, rtmean
+    real :: CE1_r3max
 
-    call read_csv_row(unit, header, nheader, ios)
-    if (ios /= 0) stop 'header read error'
 
-    print *, 'nheader = ', nheader
-    do i = 1, nheader
-        print *, 'header(', i, ') = [', trim(header(i)), ']'
-    end do
+    call get_command_argument(1, arg_year)
+    if (len_trim(arg_year) == 0) stop 'need YEAR argument'
+    read(arg_year, *) year_case
 
-    col_hrid = find_column(header, nheader, 'hrid')
-    col_lat  = find_column(header, nheader, 'lat')
-    col_lon  = find_column(header, nheader, 'lon')
+    call open_csv('fort.100', ls_data, ios)
+    if (ios /= 0) stop 'open error: fort.100'
 
-    print *, 'col_hrid = ', col_hrid
-    print *, 'col_lat  = ', col_lat
-    print *, 'col_lon  = ', col_lon
-
-    if (col_hrid < 1 .or. col_lat < 1 .or. col_lon < 1) stop 'column not found'
+    ! header skip
+    call read_csv_row(ls_data, fields, nfields, ios)
+    if (ios /= 0) stop 'read header error'
 
     do
-        call read_csv_row(unit, fields, nfields, ios)
+        call read_csv_row(ls_data, fields, nfields, ios)
         if (ios /= 0) exit
+        if (nfields < 4) cycle
 
-        print *, 'hrid = [', trim(get_field(fields, nfields, col_hrid)), ']'
-        print *, 'lat  = [', trim(get_field(fields, nfields, col_lat)), ']'
-        print *, 'lon  = [', trim(get_field(fields, nfields, col_lon)), ']'
+        dtst = trim(fields(3))
+        if (len_trim(dtst) < 4) cycle
+
+        ! year filter
+        if (to_int(dtst(1:4), -999) /= year_case) cycle
+
+        target_hrid = trim(fields(2))
+
+        call search_hrid('fort.400', trim(target_hrid), nt, hra_time, hra_lat, hra_lon, ios)
+        if (ios /= 0) then
+            print *, 'search error: ', trim(target_hrid)
+            cycle
+        end if
+
+        if (nt <= 0) then
+            print *, 'no hra steps: ', trim(target_hrid)
+            cycle
+        end if
+
+        ! safety check: hra_time year must match year_case
+        do i = 1, nt
+            if (to_int('20'//hra_time(i)(1:2), -999) /= year_case) then
+                print *, 'skip year mismatch in fort.400: ', trim(target_hrid), trim(hra_time(i))
+                nt = 0
+                exit
+            end if
+        end do
+        if (nt <= 0) cycle
+        
+        !search r3max
+        call search_hrid_stats('fort.200', trim(target_hrid), found_stats, &
+                       r1max, r3max, rtmax, racc, r1mean, r3mean, rtmean, ios)
+
+        if (.not. found_stats) then
+            print *, 'stats not found: ', trim(target_hrid)
+            cycle
+        end if
+
+        !judge CE1
+        call judge_CE1_case('bst_1h_new.txt', nt, hra_time, hra_lat, hra_lon, r3max, &
+               ce1_flag, min_dist, best_time, best_tylat, best_tylon, best_pres)
+
+        !judge CE2
+        call judge_CE2_case('bst_1h_new.txt', nt, hra_time, hra_lat, hra_lon, rtmax, &
+               ce2_flag, min_dist, best_time, best_tylat, best_tylon, best_pres)
+
+        !if criteria CE1 or CE2 meet, judge as typhoon cases
+        if ((ce1_flag == 1) .or. (ce2_flag == 1)) then
+            print *, '========================================'
+            print *, trim(target_hrid), ' : typhoon cases'
+            print *, '========================================'
+        else
+            print *, '========================================'
+            print *, trim(target_hrid), ' : not typhoon cases'
+            print *, '========================================'
+        endif
+
     end do
 
-    close(unit)
+    close(ls_data)
+
 end program main
